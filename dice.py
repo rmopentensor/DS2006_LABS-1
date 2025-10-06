457afd6b438347a05dc5437b932ab5ffa23f46aa
rmopentensor <romeomambassa@gmail.com>
2025-10-06 13:54:20 +0200

    dice.py

---
+++ dice.py
@@ -0,0 +1,20 @@
+import random
+
+
+def rollD4():
+    return random.randint(1, 4)
+
+def rollD6():
+    return random.randint(1, 6)
+
+def rollD8():
+    return random.randint(1, 8)
+
+def rollD12():
+    return random.randint(1, 12)
+
+def rollD20():
+    return random.randint(1, 20)
+
+def rollD100():
+    return random.randint(1, 100)
